2ab54aa8cca4d2d0ef2833b68d133bf6b9a876a7
dlongmta <dlongmta@gmail.com>
2016-10-20 14:37:57 +0700

    module Thuê Phòng

---
+++ DAL/DAL_HoaDon.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entity;
+using DAL;
+using System.Data;
+
+namespace DAL
+{
+    public class DAL_HoaDon
+    {
+        private KetNoiData connect = new KetNoiData();
+        public DataTable ThongTinHD(string MaHD)
+        {
+            DataTable tb = connect.GetDataTable("select * from tblHoaDon where MaHD = '" + MaHD + "'");
+            return tb;
+        }
+
+        public void ThemThongTin(EC_HoaDon EC_HD)
+        {
+            connect.ThucHienLenh("insert into tblHoaDon(MaHD, MaPhieuThue, NgayVao, NgayRa, ThanhTien) values(N'" + EC_HD.MaHD + "', N'" + EC_HD.MaPhieuThue + "', '" + EC_HD.NgayVao + "', '" + EC_HD.NgayRa + "', '" + EC_HD.ThanhTien +"') ");
+        }
+
+        public void SuaThongTin(EC_HoaDon EC_HD)
+        {
+            connect.ThucHienLenh("update tblHoaDon set NgayVao = '" + EC_HD.NgayVao + "', NgayRa = '" + EC_HD.NgayRa + "', ThanhTien = " + EC_HD.ThanhTien + " where MaHD = '" + EC_HD.MaHD + "'");
+        }
+
+        public void XoaTTHoaDon(EC_HoaDon EC_HD)
+        {
+            connect.ThucHienLenh("delete from tblHoaDon where MaHD = '" + EC_HD.MaHD + "'");
+        }
+
+        public DataTable ThongTinThanhToan(string maHd)
+        {
+            DataTable tb = connect.GetDataTable(@"select t.TenKH, t.MaPh, t.SoPhong, t.MaKH, t.MaPhieu, t.TenPT, t.DonGia, t.MaHD, t.NgayVao, t.NgayRa, t.ThanhTien , v.MaDV, v.ThoiGian, v.TenDV, v.Gia
+from (select p.MaPh, p.SoPhong, k.MaKH, k.TenKH, pt.MaPhieu,ptt.TenPT, ptt.DonGia, h.MaHD, h.NgayVao, h.NgayRa, h.ThanhTien 
+	  from tblPhong p,tblPhieuThue pt,tblKhachHang k,tblPhuongThucThue ptt,tblHoaDon h
+	  where p.MaPh = pt.MaPh and pt.MaPhieu = h.MaPhieuThue and k.MaKH = pt.MaKH
+	  and ptt.MaPT = pt.MaPT and h.MaPhieuThue = pt.MaPhieu and h.ThanhTien = 0 and h.MaHD  = '" + maHd + @"')  t 
+left join (select s.MaHD,s.MaDV, s.ThoiGian, d.TenDV, d.Gia 
+		   from tblSuDungDV s, tblDichVu d 
+		   where s.MaDV=d.MaDV ) v
+on	t.MaHD = v.MaHD ");
+            return tb;
+        }
+        public DataTable ThongTinThanhToanKH(string maKh)
+        {
+            DataTable tb = connect.GetDataTable(@"select t.TenKH, t.MaPh, t.SoPhong, t.MaKH, t.MaPhieu, t.TenPT, t.DonGia, t.MaHD, t.NgayVao, t.NgayRa, t.ThanhTien , v.MaDV, v.ThoiGian, v.TenDV, v.Gia
+from (select p.MaPh, p.SoPhong, k.MaKH, k.TenKH, pt.MaPhieu, ptt.TenPT, ptt.DonGia, h.MaHD, h.NgayVao, h.NgayRa, h.ThanhTien 
+	  from tblPhong p,tblPhieuThue pt,tblKhachHang k,tblPhuongThucThue ptt,tblHoaDon h
+	  where p.MaPh = pt.MaPh and pt.MaPhieu = h.MaPhieuThue and k.MaKH = pt.MaKH
+	  and ptt.MaPT = pt.MaPT and h.MaPhieuThue = pt.MaPhieu and h.ThanhTien = 0 and k.MaKH = '" + maKh + @"' )  t 
+left join (select s.MaHD,s.MaDV, s.ThoiGian, d.TenDV, d.Gia 
+		   from tblSuDungDV s, tblDichVu d 
+		   where s.MaDV=d.MaDV) v
+on	t.MaHD = v.MaHD ");
+            return tb;
+        }
+    }
+}
---
+++ TTN_QuanLyKhachSan/frmThuePhong.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DAL;
+using Entity;
+
+namespace TTN_QuanLyKhachSan
+{
+    public partial class frmThuePhong : Form
+    {
+        DAL_Phong dalPh = new DAL_Phong();
+        KetNoiData cn = new KetNoiData();
+        EC_PhieuThue ecPT = new EC_PhieuThue();
+        EC_HoaDon ecHD = new EC_HoaDon();
+        DAL_HoaDon dalHD = new DAL_HoaDon();
+        
+        public frmThuePhong()
+        {
+            InitializeComponent();
+        }
+        public static string findCode(string name, string columnSrc,string columnDes ,DataTable tb)
+        {
+            for (int i = 0; i < tb.Rows.Count; i++ )
+            {
+                if (tb.Rows[i][columnSrc].ToString() == name) return tb.Rows[i][columnDes].ToString();
+            }
+            return null;
+        }
+
+        private void frmThuePhong_Load(object sender, EventArgs e)
+        {
+            DateTime date = DateTime.Today.AddDays(0);
+            dtpNgayvao.Value = date;
+            //load dgvThongTin
+            DataTable tb = dalPh.ThongTinPhong("");
+            dgvThongtin.DataSource = tb;
+            for(int i = 0; i<dgvThongtin.RowCount; i++)
+            {
+                dgvThongtin.Rows[i].Cells["STT"].Value  = i+1;
+                if (dgvThongtin.Rows[i].Cells["TrangThai"].Value.ToString() == "Tốt") dgvThongtin.Rows[i].DefaultCellStyle.BackColor = Color.Lime;
+                else
+                    if (dgvThongtin.Rows[i].Cells["TrangThai"].Value.ToString() == "Đã thuê") dgvThongtin.Rows[i].DefaultCellStyle.BackColor = Color.LightSkyBlue;
+                    else
+                        if (dgvThongtin.Rows[i].Cells["TrangThai"].Value.ToString() == "Hỏng") dgvThongtin.Rows[i].DefaultCellStyle.BackColor = Color.Red;
+            }
+            //load smbKhachhang
+            tb = cn.GetDataTable("Select distinct MaKH, tenKH from tblKhachHang");
+            cmbKhachhang.DataSource = tb;
+            cmbKhachhang.DisplayMember = "tenKH";
+            txtMaKH.Text = findCode(cmbKhachhang.Text, "TenKH", "MaKH", tb);
+            
+            //load Phuongthuc
+            tb = cn.GetDataTable("Select distinct MaPT, tenPT from tblPhuongThucThue");
+            cmbPhuongthuc.DataSource = tb;
+            cmbPhuongthuc.DisplayMember= "tenPT";
+            txtMaPT.Text = findCode(cmbPhuongthuc.Text, "TenPT", "MaPT", tb);
+
+            //lay dl vao ec
+            
+            ecHD.NgayVao = date.ToShortDateString();
+            
+        }
+
+        private void cmbKhachhang_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            DataTable tb = cn.GetDataTable("Select distinct MaKH, tenKH from tblKhachHang");
+            txtMaKH.Text = findCode(cmbKhachhang.Text, "TenKH", "MaKH", tb);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < dgvThongtin.RowCount; i++)
+            {
+                dgvThongtin.Rows[i].Cells["Thue"].Value = false;
+            }
+        }
+
+        private void btnThue_Click(object sender, EventArgs e)
+        {
+            for(int i = 0; i<dgvThongtin.RowCount; i++)
+            {
+                if(Convert.ToBoolean(dgvThongtin.Rows[i].Cells["Thue"].Value)== true)
+                {
+                    if (dgvThongtin.Rows[i].Cells["TrangThai"].Value.ToString() == "Đã thuê")
+                    {
+                        MessageBox.Show("Phòng " + dgvThongtin.Rows[i].Cells["SoPhong"].Value.ToString() + " đã được thuê rồi. Chọn phòng khác!");
+                        dgvThongtin.Rows[i].Cells["Thue"].Value = false;
+                        return;
+                    }
+                    if (dgvThongtin.Rows[i].Cells["TrangThai"].Value.ToString() == "Hỏng")
+                    {
+                        MessageBox.Show("Phòng " + dgvThongtin.Rows[i].Cells["SoPhong"].Value.ToString() + " đang hỏng. Chọn phòng khác!");
+                        dgvThongtin.Rows[i].Cells["Thue"].Value = false;
+                        return;
+                    }
+                    ecPT.MaPh = dgvThongtin.Rows[i].Cells["MaPh"].Value.ToString();
+                    ecPT.MaKH = txtMaKH.Text;
+                    ecPT.MaPT = txtMaPT.Text;
+                    DateTime date = DateTime.Now;
+                    ecPT.MaPhieu = date.Day.ToString() + date.Month.ToString() + date.Hour.ToString() + date.Minute.ToString() + date.Second.ToString() + dgvThongtin.Rows[i].Cells["SoPhong"].Value.ToString();
+
+                    //thu hien CSDL bi loi o day
+                    DAL_PhieuThue dalPhieuthue = new DAL_PhieuThue();
+                    dalPhieuthue.ThemThongTin(ecPT);
+
+                    EC_Phong ecPh = new EC_Phong();
+                    ecPh.MaPh = dgvThongtin.Rows[i].Cells["MaPh"].Value.ToString();
+                    ecPh.SoPhong = dgvThongtin.Rows[i].Cells["SoPhong"].Value.ToString();
+                    ecPh.TrangThai = "Đã thuê";
+                    dalPh.SuaThongTin(ecPh);
+
+                    ecHD.MaHD = ecPT.MaPhieu;
+                    ecHD.MaPhieuThue = ecPT.MaPhieu;
+                    ecHD.ThanhTien = "0";
+                    if (ckcChuabiet.Checked == false) ecHD.NgayRa = dtpNgayra.Value.ToShortDateString();
+                    else ecHD.NgayRa = "";
+                    dalHD.ThemThongTin(ecHD);
+                }
+            }
+            MessageBox.Show("Thuê OK!");
+
+            DataTable tb = dalPh.ThongTinPhong("");
+            dgvThongtin.DataSource = tb;
+
+            for (int i = 0; i < dgvThongtin.RowCount; i++)
+            {
+                dgvThongtin.Rows[i].Cells["STT"].Value = i + 1;
+                if (dgvThongtin.Rows[i].Cells["TrangThai"].Value.ToString() == "Tốt") dgvThongtin.Rows[i].DefaultCellStyle.BackColor = Color.Lime;
+                else
+                    if (dgvThongtin.Rows[i].Cells["TrangThai"].Value.ToString() == "Đã thuê") dgvThongtin.Rows[i].DefaultCellStyle.BackColor = Color.LightSkyBlue;
+                    else
+                        if (dgvThongtin.Rows[i].Cells["TrangThai"].Value.ToString() == "Hỏng") dgvThongtin.Rows[i].DefaultCellStyle.BackColor = Color.Red;
+            }
+        }
+
+        private void cmbPhuongthuc_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            DataTable tb = cn.GetDataTable("Select distinct MaPT, tenPT from tblPhuongThucThue");
+            txtMaPT.Text = findCode(cmbPhuongthuc.Text, "TenPT", "MaPT", tb);
+            if (cmbPhuongthuc.Text == "Qua đêm")
+            {
+                DateTime dateOut = dtpNgayvao.Value;
+                dateOut = dateOut.AddDays(1);
+                dtpNgayra.Value = dateOut;
+                ecHD.NgayRa = dateOut.ToShortDateString();
+                dtpNgayra.Enabled = false;
+                ckcChuabiet.Enabled = false;
+            }
+            else
+            {
+                dtpNgayra.Enabled = true;
+                ckcChuabiet.Enabled = true;
+            }
+
+        }
+
+        private void ckcChuabiet_CheckedChanged(object sender, EventArgs e)
+        {
+            if (ckcChuabiet.Checked == false) ecHD.NgayRa = dtpNgayra.Value.ToShortDateString();
+            else ecHD.NgayRa = "";
+        }
+    }
+}
